c096c5aa4647b0baa0117ef36e4762e5b091f5c6
Nate D <nate@idontchop.com>
2020-03-22 11:25:06 -0700

    comments todo

--- src/main/java/com/idontchop/blocks/BlocksApplication.java
+++ src/main/java/com/idontchop/blocks/BlocksApplication.java
@@ -3,6 +3,17 @@ package com.idontchop.blocks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * TODO:
+ * 
+ *  After brainstorming, determined that blocks should be written twice, once in who someone blocked
+ *  and who blocked the user. Would mean each document has a blocked and isBlocked field.
+ *  
+ *  Duplication of data and slower writes but would make reads super quick.
+ *  
+ * @author micro
+ *
+ */
 @SpringBootApplication
 public class BlocksApplication {
 
